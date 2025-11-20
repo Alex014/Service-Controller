eeec6b57a80c47d9b0227894a5eaae8862e370a1
Aleksej Sokolov <aleksej000@gmail.com>
2025-11-20 05:55:17 +0200

    Service names

--- run.py
+++ run.py
@@ -10,12 +10,19 @@ class GLOBAL:
 
 if not os.path.exists(GLOBAL.services):
     with open(GLOBAL.services, 'w+') as file:
+        # data = {
+        #     "mysql": { "status": "running", "command": "", "icon": "fas fa-user-shield", "style": "background: linear-gradient(135deg, var(--primary), #4895ef);", "log": "" },
+        #     "tor": { "status": "running", "command": "", "icon": "fas fa-search", "style": "background: linear-gradient(135deg, var(--secondary), #3a0ca3);","log": "" },
+        #     "cups": { "status": "running", "command": "", "icon": "fab fa-bitcoin", "style": "background: linear-gradient(135deg, #7209b7, #560bad);","log": "" },
+        #     "ufw": { "status": "running", "command": "", "icon": "fas fa-network-wired", "style": "background: linear-gradient(135deg, #4cc9f0, var(--primary));","log": "" },
+        #     "AmneziaVPN": { "status": "running", "command": "", "icon": "fas fa-file-upload", "style": "background: linear-gradient(135deg, var(--accent), #b5179e);","log": "" }
+        # }
         data = {
-            "mysql": { "status": "running", "command": "", "icon": "fas fa-user-shield", "style": "background: linear-gradient(135deg, var(--primary), #4895ef);", "log": "" },
-            "tor": { "status": "running", "command": "", "icon": "fas fa-search", "style": "background: linear-gradient(135deg, var(--secondary), #3a0ca3);","log": "" },
-            "cups": { "status": "running", "command": "", "icon": "fab fa-bitcoin", "style": "background: linear-gradient(135deg, #7209b7, #560bad);","log": "" },
-            "ufw": { "status": "running", "command": "", "icon": "fas fa-network-wired", "style": "background: linear-gradient(135deg, #4cc9f0, var(--primary));","log": "" },
-            "AmneziaVPN": { "status": "running", "command": "", "icon": "fas fa-file-upload", "style": "background: linear-gradient(135deg, var(--accent), #b5179e);","log": "" }
+            "ness": { "status": "running", "command": "", "icon": "fas fa-user-shield", "style": "background: linear-gradient(135deg, var(--primary), #4895ef);", "log": "" },
+            "explorer": { "status": "running", "command": "", "icon": "fas fa-search", "style": "background: linear-gradient(135deg, var(--secondary), #3a0ca3);","log": "" },
+            "emercoin": { "status": "running", "command": "", "icon": "fab fa-bitcoin", "style": "background: linear-gradient(135deg, #7209b7, #560bad);","log": "" },
+            "ipfs": { "status": "running", "command": "", "icon": "fas fa-network-wired", "style": "background: linear-gradient(135deg, #4cc9f0, var(--primary));","log": "" },
+            "wsftp": { "status": "running", "command": "", "icon": "fas fa-file-upload", "style": "background: linear-gradient(135deg, var(--accent), #b5179e);","log": "" }
         }
 
         file.write(json.dumps(data))
